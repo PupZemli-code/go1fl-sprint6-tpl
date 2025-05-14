9fc26090f0b9a9656c03bd57d15c64bf2955e5f1
PupZemli-code <m.i.s.h.a.9611@mail.ru>
2025-05-14 14:52:56 +0300

    commit 2 Переделал пакет service через startings.ContainsFunc, а так же перенес initLogger из servir в main

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"io"
+	"log"
+	"os"
+
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
 )
 
+func initLogger() *log.Logger {
+	// Создаем файл для логов
+	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Failed to open log file: %v", err)
+	}
+
+	// Настраиваем форматирование логов
+	logger := log.New(file, "LOG: ", log.Ldate|log.Ltime|log.Lmicroseconds) // Добавляем дату и время
+	io.MultiWriter(file, os.Stdout)                                         // Пишем в файл и консоль
+
+	return logger
+}
+
 func main() {
 	// Создаем логгер
-	logger := server.InitLogger()
+	logger := initLogger()
 
 	// Создаем сервер
 	srv := server.NewServer(logger)
--- internal/service/service.go
+++ internal/service/service.go
@@ -8,23 +8,14 @@ import (
 )
 
 func Service(msg string) (string, error) {
-	var morseText bool
+
 	if msg == "" {
 		return "", errors.New("файл пуст")
 	}
 
-	if strings.Contains(string(msg), "..") ||
-		strings.Contains(string(msg), "--") ||
-		strings.Contains(string(msg), "-.") ||
-		strings.Contains(string(msg), ".-") {
-		morseText = true
-	}
-
-	if morseText {
-		out := morse.ToText(msg)
-		return out, nil
+	if strings.ContainsFunc(msg, func(r rune) bool { return r == '.' || r == '-' }) {
+		return morse.ToText(msg), nil
 	} else {
-		out := morse.ToMorse(msg)
-		return out, nil
+		return morse.ToMorse(msg), nil
 	}
 }
